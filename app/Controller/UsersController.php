16209e50f5dcc17de26b0b05d99a2728a0622fff
Xuân Hợp Đỗ <Xun Hp >
2017-04-14 19:21:59 +0700

    Init project

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xuan_Hop
+ * Date: 4/11/2017
+ * Time: 7:46 AM
+ */
+class UsersController extends AppController{
+    public $name="Users";
+    function index(){
+
+    }
+}
+?>
