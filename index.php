faf80fc295aa0797f14e9d7e42703e6c343d691a
Quynh <quynhquynh.hn1997@gmail.com>
2017-04-21 20:48:51 +0700

    first commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Requests collector.
- *
+ * 
  *  This file collects requests if:
  *	- no mod_rewrite is available or .htaccess files are not supported
  *  - requires App.baseUrl to be uncommented in app/Config/core.php
